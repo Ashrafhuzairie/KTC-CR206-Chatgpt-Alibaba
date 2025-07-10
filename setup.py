ffbb40327ab8fd2f20506c045b9f8190ca6fe306
Ashrafhuzairie <ashrafhuzairie@gmail.com>
2025-07-10 11:55:31 +0800

    Structure Folder Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup (
+    name = 'KTC-ACC CR206-Chatgpt',
+    version = '0.0.0',
+    author = 'Mohd Ashraf Huzairie',
+    author_email = 'md.ashraf@ktcgroup.com.sg',
+    packages = find_packages(),
+    install_requires = []
+
+)
